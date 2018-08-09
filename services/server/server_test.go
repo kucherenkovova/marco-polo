1c95d28c47eaea5029d470d762005409efa5e26a
Mike Faraponov <mikefaraponov@users.noreply.github.com>
2018-08-09 11:36:17 +0300

    Added test cases for server (#5)
    * Added test cases for server
    
    * Added test case for unexpected word

---
+++ services/server/server_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	"github.com/kucherenkovova/marco-polo/proto"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Server", func() {
+	dict := map[string]string{
+		"monkey": "follow",
+		"follow": "monkey",
+	}
+	mps := &MarcoPoloServer{dict}
+	It("should return follow for monkey", func() {
+		phrase := &proto.Phrase{Word: "monkey"}
+		result, err := mps.Send(context.Background(), phrase)
+		Expect(err).To(BeNil())
+		Expect(result.GetWord()).To(Equal("follow"))
+	})
+	It("should return monkey for follow", func() {
+		phrase := &proto.Phrase{Word: "follow"}
+		result, err := mps.Send(context.Background(), phrase)
+		Expect(err).To(BeNil())
+		Expect(result.GetWord()).To(Equal("monkey"))
+	})
+	It("should return empty string for random word", func() {
+		phrase := &proto.Phrase{Word: "random word"}
+		result, err := mps.Send(context.Background(), phrase)
+		Expect(err).To(BeNil())
+		Expect(result.GetWord()).To(Equal(""))
+	})
+})
